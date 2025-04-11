4d4c3a04213fa1233f9c634507fe3f29900d609a
Umut Konmus <umutkonmus@gmail.com>
2025-04-12 01:51:42 +0300

    Mapping profile hotfix*

--- DirectoryService/Mapper/MappingProfile.cs
+++ DirectoryService/Mapper/MappingProfile.cs
@@ -13,15 +13,20 @@ namespace DirectoryService.Mapper
             #region Person
             CreateMap<Person, PersonDTO>().ReverseMap();
             CreateMap<Person, CreatePersonDTO>().ReverseMap();
-            CreateMap<Person, PersonWithContactInfoDTO>().ReverseMap();
+
+            CreateMap<Person, PersonWithContactInfoDTO>()
+                .ForMember(dest => dest.ContactInfos, opt => opt.MapFrom(src => src.ContactInfos));
             #endregion
+
             #region ContactInfo
             CreateMap<ContactInfo, ContactInfoDTO>().ReverseMap();
             CreateMap<ContactInfo, CreateContactInfoDTO>().ReverseMap();
+
+            // Bu mapping, ContactInfo içindeki Type.Name'i TypeName olarak mapler.
             CreateMap<ContactInfo, ContactInfoForPersonDTO>()
-                .ForMember(dest => dest.TypeName, opt => opt.MapFrom(src => src.Type.Name))
-                .ReverseMap();
+                .ForMember(dest => dest.TypeName, opt => opt.MapFrom(src => src.Type.Name));
             #endregion
+
             #region ContactType
             CreateMap<ContactType, ContactTypeDTO>().ReverseMap();
             CreateMap<ContactType, CreateContactTypeDTO>().ReverseMap();
