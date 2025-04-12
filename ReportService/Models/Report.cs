16a157ab1b84e13d99b7e2512caa55eedf4c3218
Umut Konmus <umutkonmus@gmail.com>
2025-04-12 16:50:20 +0300

    Report service created

---
+++ ReportService/Models/Report.cs
@@ -0,0 +1,18 @@
+﻿namespace ReportService.Models
+{
+    public sealed class Report
+    {
+        public Guid ID { get; set; }
+        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
+        public ReportStatus Status { get; set; } = ReportStatus.Preparing;
+        public string Location { get; set; }
+        public int PersonCount { get; set; }
+        public int PhoneNumberCount { get; set; }
+    }
+
+    public enum ReportStatus
+    {
+        Preparing = 0,
+        Completed = 1
+    }
+}
