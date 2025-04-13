bcd99ffc0cabbc10d0171dd2382cf290f643715a
Umut Konmus <umutkonmus@gmail.com>
2025-04-13 23:45:38 +0300

    Automapper implemented.

---
+++ ReportService/Mapper/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ReportService.DTOs.Report;
+using ReportService.Models;
+
+namespace ReportService.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Report, ReportDTO>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<Report, ReportWithDetailsDTO>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ReverseMap();
+
+            CreateMap<ReportDetail, ReportDetailDTO>()
+                .ReverseMap();
+        }
+    }
+}
