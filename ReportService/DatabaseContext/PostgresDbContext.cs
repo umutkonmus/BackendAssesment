daa7e58a1acaab56db5ccbc9ca5e1caf47719561
Umut Konmus <umutkonmus@gmail.com>
2025-04-13 23:46:52 +0300

    Name changed.

---
+++ ReportService/DatabaseContext/PostgresDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportService.Models;
+
+namespace ReportService.DatabaseContext
+{
+    public class PostgresDbContext : DbContext
+    {
+        public PostgresDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<ReportDetail> ReportDetails { get; set; }
+    }
+}
