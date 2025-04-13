7d9eea297559d00c0391ba853f61c2a94a4f2cf6
Umut Konmus <umutkonmus@gmail.com>
2025-04-13 23:46:40 +0300

    Report model modified.

--- ReportService/Models/Report.cs
+++ ReportService/Models/Report.cs
@@ -1,14 +1,13 @@
-﻿using ReportService.Utils;
+﻿using ReportService.Models.Abstract;
+using ReportService.Utils;
 
 namespace ReportService.Models
 {
-    public sealed class Report
+    public class Report : IEntity
     {
         public Guid ID { get; set; }
         public DateTime RequestedAt { get; set; } 
         public ReportStatus Status { get; set; } 
-        public string Location { get; set; }
-        public int PersonCount { get; set; }
-        public int PhoneNumberCount { get; set; }
+        public virtual List<ReportDetail> ReportDetails { get; set; }
     }
 }
