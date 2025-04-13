62c454354232d0b63c81c1f9ab1f81d78d3f1e1a
Umut Konmus <umutkonmus@gmail.com>
2025-04-13 23:40:21 +0300

    Report controller name changed.

---
+++ DirectoryService/Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using DirectoryService.DTOs.Report;
+using DirectoryService.Services.Abstracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class ReportsController : ControllerBase
+    {
+        private readonly IKafkaProducerService _producerService;
+        public ReportsController(IKafkaProducerService producerService)
+        {
+            _producerService = producerService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RequestReport([FromBody] ReportRequestDTO request)
+        {
+            await _producerService.SendReportRequestAsync(request);
+            return Ok("Report request sent.");
+        }
+    }
+}
